b2aa0139e6f86daaa700c8a5629fc0695ed45e46
Janyson Marques <janyson29@hotmail.com>
2025-12-04 18:16:40 -0300

    adicionando botao

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,13 @@
 import { Image, View } from 'react-native';
+import { Button } from '@/components/Button';
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image source={require("../../assets/logo.png")} style={styles.logo} />
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <Button />
     </View>
   );
 }
