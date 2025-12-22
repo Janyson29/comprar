87daf7df60205e98fef7a8cc2f1e96890e889545
Janyson Marques <janyson29@hotmail.com>
2025-12-22 18:05:31 -0300

    add icons

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,15 +1,25 @@
 import { Image, View } from 'react-native';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
+import { Filter } from '@/components/Filter';
+
 import { styles } from './styles';
+import { FilterStatus } from '@/types/FilterStatus';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <Input placeholder="O que você precisa comprar?" />
-      <Button title="Entrar" />
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}>
+        <Filter status={FilterStatus.DONE} isActive />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
+      </View>
     </View>
   );
 }
